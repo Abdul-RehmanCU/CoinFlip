012dcc42d1295d3c4fa92397ca8a2def6c99b9f2
Abdul-RehmanCU <sonubhai883@gmail.com>
2025-01-23 22:00:30 -0500

    first

---
+++ flip/src/App.js
@@ -0,0 +1,14 @@
+import react from 'react'
+import FlipCoin from './components/FlipCoin'
+
+
+function App() {
+  
+    return (
+      <div className="App">
+        <FlipCoin />
+      </div>
+    );
+  }
+  
+  export default App;
